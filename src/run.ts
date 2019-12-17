4010981afcfdc8fec775a2d116331507da0dfe85
Andre Staltz <andre@staltz.com>
2019-12-17 18:04:07 +0200

    fix top-level run() api's layout argument type

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,4 @@
-import {Navigation} from 'react-native-navigation';
+import {Navigation, Layout, Options} from 'react-native-navigation';
 import {Screens} from './types';
 import makeComponent from './makeComponent';
 import {setupReusable, Drivers} from '@cycle/run';
@@ -6,8 +6,8 @@ import {setupReusable, Drivers} from '@cycle/run';
 export function run<D extends Drivers>(
   screens: Screens,
   drivers: D,
-  layout: any,
-  defaultOpts?: any,
+  layout: Layout,
+  defaultOpts?: Options,
 ): void {
   const engine = setupReusable(drivers);
   for (const [id, main] of Object.entries(screens)) {
@@ -15,6 +15,6 @@ export function run<D extends Drivers>(
   }
   Navigation.events().registerAppLaunchedListener(() => {
     if (defaultOpts) Navigation.setDefaultOptions(defaultOpts);
-    Navigation.setRoot(layout);
+    Navigation.setRoot({root: layout});
   });
 }
